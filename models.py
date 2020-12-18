7ce0393661fdc79bc094e2636209b1e115a6d6e5
khadija2323 <doj1111@hotmail.com>
2020-12-19 00:41:41 +0300

    models.py complete

---
+++ models.py
@@ -0,0 +1,95 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+#from dotenv import load_dotenv
+
+
+#load_dotenv()
+
+database_path = os.environ['DATABASE_URL']
+if not database_path:
+    database_name = "capstone_agency"
+    database_path = "postgresql://{}:{}@{}/{}".format('postgres',
+                                                      'admin',
+                                                      'localhost:5432',
+                                                      database_name)
+
+db = SQLAlchemy()
+
+
+def setup_db(app, database_path=database_path):
+    app.config.from_object('config')
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    db.app = app
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    #db.create_all()
+
+
+class Actor(db.Model):
+    __tablename__ = 'actor'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.String)
+    gender = db.Column(db.String)
+
+    def __repr__(self):
+        return f"<Actor id='{self.id}' name='{self.name}'>"
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    release = db.Column(db.String)
+
+    def __repr__(self):
+        return f"<Movie id='{self.id}' title='{self.title}'>"
+
+    def __init__(self, title, release):
+        self.title = title
+        self.release = release
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release': self.release,
+        }
